afff5324604e62e5d2d734f95a6ec02b609d60dc
alkaitagi <alkaitagi@outlook.com>
2021-08-27 16:30:04 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="cols">
+    <textarea v-model="inputA" placeholder="language A" />
+    <textarea v-model="inputB" placeholder="language B" />
+    <textarea v-model="output" readonly />
+  </div>
+</template>
+
+<script setup lang="ts">
+import leven from "leven";
+import { computed, ref } from "vue";
+
+function zip<T>(a: T[], b: T[]) {
+  return Array.from(Array(Math.max(b.length, a.length)), (_, i) => [
+    a[i],
+    b[i],
+  ]);
+}
+
+const inputA = ref("");
+const inputB = ref("");
+const output = computed(() => {
+  const pairs = zip(inputA.value.split("\n"), inputB.value.split("\n"));
+  return pairs.reduce((sum, [a, b]) => sum + leven(a, b), 0) / pairs.length;
+});
+</script>
+
+<style>
+html,
+body,
+#app {
+  height: 90%;
+
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.cols,
+.cols * {
+  height: 512px;
+}
+</style>
